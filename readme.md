597b39e799c6c60597967daa8c64acc4eb0994be
Siarhei Mitasau <darkenRahl1893@gmail.com>
2019-07-03 23:02:51 +0300

    init

---
+++ readme.md
@@ -0,0 +1,40 @@
+worklog:
+Commits on Jun 24, 2019
+    feat: add some btn listeners for test
+    @SergMitMin
+    SergMitMin committed 9 days ago
+
+fead: implement drawLine feature
+@SergMitMin
+SergMitMin committed 10 days ago
+Commits on Jun 21, 2019
+
+    feat: implement drawDot method
+    @SergMitMin
+    SergMitMin committed 13 days ago
+
+Commits on Jun 10, 2019
+
+    docs: create project template
+    @SergMitMin
+    SergMitMin committed 23 days ago
+
+Commits on Jun 9, 2019
+
+    docs: install webpack
+    @SergMitMin
+    SergMitMin committed 24 days ago
+
+Commits on Jun 8, 2019
+
+    docs: add reamde.md, package.json files
+    @SergMitMin
+    SergMitMin committed 25 days ago
+
+Commits on Apr 30, 2019
+
+    fix: fix error with keypress event
+    @SergMitMin
+    SergMitMin committed on 30 Apr
+
+document: create main folder and docs 
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    inject: false,
+    title: 'Piskel Clone',
+    template: './index.html',
+  })],
+};
